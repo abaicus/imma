0919ba84e672efb2349f441a966dc8783223dc5a
cristian-ungureanu <cristian@themeisle.com>
2017-03-22 22:46:08 +0200

    start blog

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,26 +10,25 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+	<?php
+	$class_to_add = 'col-md-5';
+	if( ! has_post_thumbnail() ){
+		$class_to_add = 'col-md-12';
+	}
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php imma_posted_on(); ?>
-		</div><!-- .entry-meta -->
+	if( has_post_thumbnail() ){?>
+		<header class="entry-header col-md-5 col-sm-12 col-xs-12 blog-post-image">
+			<img src="<?php the_post_thumbnail_url('imma-blog-size'); ?>" alt="">
+		</header><!-- .entry-header -->
 		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
+	}?>
+	<div class="entry-content col-sm-12 col-xs-12 blog-post-content <?php echo esc_attr( $class_to_add ); ?>">
 		<?php
+		if ( is_single() ) { ?>
+			<h2 class="post-title"><?php the_title(); ?></h2>
+			<?php
 			the_content( sprintf(
-				/* translators: %s: Name of current post. */
+			/* translators: %s: Name of current post. */
 				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'imma' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
@@ -38,10 +37,18 @@
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'imma' ),
 				'after'  => '</div>',
 			) );
+		} else { ?>
+			<h2 class="post-title"><?php the_title(); ?></h2>
+
+			<p class="text-content">
+				<?php the_excerpt(); ?>
+			</p>
+			<?php
+		}
 		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php imma_entry_footer(); ?>
+		<?php //imma_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
